32edd16b2c1b045dcc2aebf9719d466cf8daa736
IamTester <94913347+coderAshaan@users.noreply.github.com>
2023-06-24 08:23:25 +0530

    Added files via uploading

---
+++ CarArray.java
@@ -0,0 +1,12 @@
+public class CarArray {
+	
+    public static void main(String[] args) { 
+	
+	String cars[] = {"BMW", "Ambassadow", "Honda", "Hyundai"};
+	System.out.println (" Car at 3rd index: " +cars[3]);
+	System.out.println("Car using for loop");
+	for(int k = 0; k < 4 ; k ++) {
+		System.out.println(cars[k]);
+	}
+}
+}
